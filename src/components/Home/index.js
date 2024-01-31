4869c0561e20b2592edc2162ffce82c30c39f087
venkateshsure <venky007sure@gmai.com>
2024-01-31 14:48:04 +0000

    implemented Home Component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,37 @@
+import {Component} from 'react'
+
 import './index.css'
 
-const Home = () => <h1>Home</h1>
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+class Home extends Component {
+  state = {status: apiStatusConstants.initial}
+
+  componentDidMount = () => {
+    this.getCovidData()
+  }
+
+  getCovidData = async () => {
+    this.setState({status: apiStatusConstants.inProgress})
+    const options = {
+      method: 'GET',
+    }
+    const response = await fetch(
+      'https://apis.ccbp.in/covid19-state-wise-data',
+      options,
+    )
+    const responseData = await response.json()
+    console.log(responseData)
+  }
+
+  render() {
+    return <h1>home</h1>
+  }
+}
 
 export default Home
