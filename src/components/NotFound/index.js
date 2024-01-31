2fdeba4b6acf65c1bfbf27a1558ad43cfcb94773
venkateshsure <venky007sure@gmai.com>
2024-01-31 05:21:22 +0000

    implemented not found

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,5 +1,24 @@
 import './index.css'
 
-const NotFound = () => <h1>Not found</h1>
+const NotFound = () => (
+  <div className="not-found-head-con">
+    <div className="not-found-con">
+      <img
+        src="https://res.cloudinary.com/dzfhyklmn/image/upload/v1706675253/Vector_po3ak8.png"
+        alt="not-found"
+      />
+      <h1 className="not-found-heading">PAGE NOT FOUND</h1>
+      <p className="not-found-desc">
+        we’re sorry, the page you requested could not be found Please go back to
+        the homepage
+      </p>
+      <div className="not-found-btn-con">
+        <button className="not-found-btn" type="button">
+          Home
+        </button>
+      </div>
+    </div>
+  </div>
+)
 
 export default NotFound
